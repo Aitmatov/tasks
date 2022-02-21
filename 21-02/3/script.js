ca464006039fddf07f3f99a17e1110c8c38814e5
Melis <melis.aitmatov@gmail.com>
2022-02-21 19:42:38 +0600

    feat: added finish  21-02 all folder

---
+++ 21-02/3/script.js
@@ -0,0 +1,19 @@
+let tax1 = 100;
+let tax2 = 200;
+let result  = tax1 > 80 && 200 < 210;
+console.log(result); // true
+
+let tax3 = 301;
+let income = 201;
+let result1 = tax3 === 301 && income === 200;
+console.log(result1);
+
+let tax4 = 400;
+let tax5 = 500;
+let result2 = tax4 < 600 && tax5 > 700;
+console.log(result2);
+
+let income1 = 200;
+let income3 = 400;
+let result3 = income1 < 199 &&  income3 > 500;
+console.log(result3);
---
+++ 21-02/4/script.js
@@ -0,0 +1,19 @@
+let incom = 150;
+let debt = 200;
+let result =  incom > 200 || debt < 300;
+console.log(result);
+
+
+let consomption = 500;
+let income1 = 200;
+let result1 = !(consomption > 600) || (income1 ===100);
+console.log(result1);
+
+let tax1 = 300;
+let tax2 =400;
+let result2 = tax1 ===200 || !(400 > tax2 );
+console.log(result2);
+ let income2 = 555;
+ let consomtion2 = 620;
+ let result4 = income2 > 700 || consomtion2 < 100;
+ console.log(result4);
