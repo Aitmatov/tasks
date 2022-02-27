e641ca9033a186d44e4fd79e34097162f0b961d3
Melis <melis.aitmatov@gmail.com>
2022-02-27 22:29:32 +0600

    feat: add finished  25-02 all folder

--- 25-02/1/script.js
+++ 25-02/1/script.js
@@ -1,8 +1,8 @@
 /*1) Создайте массив с числами. Перебирая элементы созданного массива с помощью
 цикла выведите только те элементы массива, которые больше нуля и меньше 10.*/
-let number =10;
-for( let i = 0; i < 10; i++){
-    console.log(i);
+let number3 =[2,3,5,6,9,7,1,4,8,9,10];
+for( let i = 0; i < number3.length; i++){
+    console.log(number3[i]);
 }
 /*2) У нас есть массив с элементами 1, 2, 5, 6, 88, 5.
 С помощью цикла надо найти сумму элементов этого массива.*/
@@ -18,15 +18,54 @@ for(let i = 0; i < 10; i++){
     console.log(number1[5]**2);
 }
 
+
+
+
+/******* №4********/
 let otvet = prompt('Who is come?');
+    if(otvet === 'Admin'){
+        alert('Welcome!');
+    }else if(otvet === "null" ){
+        alert('I do not know you');
+    }else{
+        alert('I am sorry');
+    }
 let otvet1 =prompt('pasword');
-    if(otvet === 'Admin' && otvet1 === 'melis89'){
-        alert('Welcome');
-    } else if( 'other') {
+    if(otvet1 ==='good123'){
+        alert("Goodman")
+    }else if(otvet1 === 'null'){
+        alert("I did not understand")
+    }else{
+        alert("haha")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+if(otvet === 'Admin'){
+        alert('Welcome!');
+    }
+    if(otvet1 === 'melis89'){
+    alert('Are you clever');
+    } else if(otvet === "null" ){
         alert('I do not know you');
     } else{
         alert('I am sorry');
-    }
+    }else if(otvet1 === 'null') {
+        alert('entry canceled');
+    } else(otvet1 === 'null'){
+        alert("error password ");
+   }
+*/
 
 
 
